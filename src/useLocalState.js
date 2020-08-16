679607dfea5fbae1417585c6e857a75480b8f2e9
Jon Q <hello@jonquach.com>
2020-08-16 17:42:51 -0400

    Fix useLocalState export

--- src/useLocalState.js
+++ src/useLocalState.js
@@ -5,7 +5,7 @@
 
 import { useState } from 'react';
 
-export function useLocalStorage(key, initialValue) {
+export function useLocalState(key, initialValue) {
 	// State to store our value
 	// Pass initial state function to useState so logic is only executed once
 	const [storedValue, setStoredValue] = useState(() => {
